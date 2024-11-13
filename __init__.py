cb66e0d9498abb4e55fb7eddbf9bff746c43bb60
rinsanga <raltelalrinsanga@mediamonks.com>
2024-11-13 09:27:56 +0000

    added blip img captioner

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
 from .florence2coordinatesbutxy import Florence2toCoordinatesButxy
-from .florence3img2txt import Florence3img2txt
+from .BlipImgCaptioning import BlipImgCaptioning
 
 NODE_CLASS_MAPPINGS = {
     "Florence2toCoordinatesButxy": Florence2toCoordinatesButxy,
-    "Florence3img2txt": Florence3img2txt,
+    "BlipImgCaptioning": BlipImgCaptioning,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Florence2toCoordinatesButxy": "Florence2 Coordinates (XY Split)",
-    "Florence3img2txt": "Florence3 Image to Text",
+    "BlipImgCaptioning": "BLIP Image Captioning",
 }
